a1b3fc1ece14bbd0045165ba790e5f39d66d5268
yaincoding <yaincoding@gmail.com>
2022-07-27 23:48:09 +0900

    class -> data class

--- src/main/kotlin/com/yaincoding/yaco_fashion/dto/goods/SearchGoodsResponseDto.kt
+++ src/main/kotlin/com/yaincoding/yaco_fashion/dto/goods/SearchGoodsResponseDto.kt
@@ -2,7 +2,7 @@ package com.yaincoding.yaco_fashion.dto.goods
 
 import com.yaincoding.yaco_fashion.document.goods.GoodsDocument
 
-class SearchGoodsResponseDto (
+data class SearchGoodsResponseDto (
     val count: Int,
     val docs: List<GoodsDocument>
 )
