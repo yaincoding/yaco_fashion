209551f09a57a4ddc4cd00db834ab37bc8d7e1c6
yaincoding <yaincoding@gmail.com>
2022-07-31 13:39:35 +0900

    es query 전체적인 구조 변경

--- src/main/kotlin/com/yaincoding/yaco_fashion/query_dsl/QueryDslFactory.kt
+++ src/main/kotlin/com/yaincoding/yaco_fashion/query_dsl/QueryDslFactory.kt
@@ -1,34 +1,51 @@
 package com.yaincoding.yaco_fashion.query_dsl
 
-import com.yaincoding.yaco_fashion.dto.goods.SearchGoodsRequestDto
-
 class QueryDslFactory {
 
     companion object {
         fun createEsQuery(params: EsQueryParams): String {
-            val should = buildShould(params.query)
+            val should = buildShould(params.query).joinToString(",", "[", "]")
+            val scoreFunctions = buildScoreFunctions().joinToString(",", "[", "]")
+            val sortQuery = params.sort.getSortQuery()
+
             val esQuery = """
                 {
                     "query": {
-                        "bool": {
-                            "should": ${should.joinToString(",", "[", "]")},
-                            "minimum_should_match": 1
+                        "function_score": {
+                            "query": {
+                                "constant_score": {
+                                    "filter": {
+                                        "bool": {
+                                            "should": $should
+                                        }
+                                    }
+                                }
+                            },
+                            "functions": $scoreFunctions
                         }
-                    }
+                    },
+                    "sort": $sortQuery
                 }
             """
 
             return esQuery
         }
 
-        fun buildShould(query: String?): List<String> {
+        private fun buildShould(query: String?): List<String> {
             val should = mutableListOf<String>()
             query?.let {
                 should.add(
                     """
                         {
-                            "match": {
-                                "title": "$query"
+                            "multi_match": {
+                                "query": "$query",
+                                "type": "cross_fields",
+                                "operator": "and",
+                                "analyzer": "search_analyzer",
+                                "fields": [
+                                    "title",
+                                    "hash_tags"
+                                ]
                             }
                         }
                     """
@@ -36,6 +53,46 @@ class QueryDslFactory {
             }
 
             return should
+        }
+
+        private fun buildScoreFunctions(): List<String> {
+            val functions: MutableList<String> = mutableListOf()
+            functions.add(
+                """
+                    {
+                        "field_value_factor": {
+                            "field": "click_count",
+                            "factor": 1,
+                            "missing": 1
+                        }
+                    }
+                """
+            )
+
+            functions.add(
+                """
+                   {
+                        "field_value_factor": {
+                            "field": "like_count",
+                            "factor": 2,
+                            "missing": 1
+                        }
+                   } 
+                """
+            )
+
+            functions.add(
+                """
+                   {
+                        "field_value_factor": {
+                            "field": "sell_count",
+                            "factor": 3,
+                            "missing": 1
+                        }
+                   } 
+                """
+            )
+            return functions
         }
     }
 }
