53cbbfcba24c5359e7b70dcf9d4a1ce409bb4589
yaincoding <yaincoding@gmail.com>
2022-09-06 22:36:46 +0900

    page default 0 -> 1

--- src/main/kotlin/com/yaincoding/yaco_fashion/domain/goods/dto/SearchGoodsRequestDto.kt
+++ src/main/kotlin/com/yaincoding/yaco_fashion/domain/goods/dto/SearchGoodsRequestDto.kt
@@ -4,6 +4,6 @@ data class SearchGoodsRequestDto (
     val query: String?,
     val sort: String = "MATCH",
     val categoryId: Int? = null,
-    val page: Int = 0,
+    val page: Int = 1,
     val size: Int = 10,
 )
