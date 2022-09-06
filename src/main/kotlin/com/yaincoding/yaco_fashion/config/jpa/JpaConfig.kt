1dc1094650dca687ea72aa693759aa563af570c7
yaincoding <yaincoding@gmail.com>
2022-09-06 22:38:25 +0900

    spring data jpa config

---
+++ src/main/kotlin/com/yaincoding/yaco_fashion/config/jpa/JpaConfig.kt
@@ -0,0 +1,9 @@
+package com.yaincoding.yaco_fashion.config.jpa
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig{
+}
