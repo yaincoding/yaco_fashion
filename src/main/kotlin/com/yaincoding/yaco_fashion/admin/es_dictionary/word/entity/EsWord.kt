9d8e25a752c71e74444d970d59d020a5e92cef37
yaincoding <yaincoding@gmail.com>
2022-09-08 22:56:00 +0900

    renamed

--- src/main/kotlin/com/yaincoding/yaco_fashion/admin/es_dictionary/word/entity/EsWord.kt
+++ src/main/kotlin/com/yaincoding/yaco_fashion/admin/es_dictionary/word/entity/EsWord.kt
@@ -1,22 +1,23 @@
-package com.yaincoding.yaco_fashion.admin.dictionary.goods
+package com.yaincoding.yaco_fashion.admin.es_dictionary.word.entity
 
 import com.yaincoding.yaco_fashion.common.BaseTime
 import javax.persistence.*
 
 @Entity
-@Table(name = "goods_synonym")
-class GoodsSynonymDictionary: BaseTime() {
+@Table(name = "goods_word")
+class EsWord (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int? = null
+    val id: Long? = null,
 
     @Column(name = "word", nullable = false)
-    var word: String? = null
+    var word: String? = null,
 
-    @Column(name = "synonym", nullable = false)
-    var synonym: String? = null
+    @Column(name = "expression", nullable = true)
+    var expression: String? = null,
 
     @Column(name = "active", nullable = false)
     var active: Boolean = true
-}
+
+): BaseTime()
