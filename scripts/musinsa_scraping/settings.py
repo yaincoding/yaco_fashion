dc1be30eea700e34f047e3ff1712106b3c5cea3e
yaincoding <yaincoding@gmail.com>
2022-09-12 18:55:13 +0900

    db config 삭제

--- scripts/musinsa_scraping/settings.py
+++ scripts/musinsa_scraping/settings.py
@@ -7,12 +7,6 @@ BASE_CONFIG = {
 
 S3_CONFIG = {
     'BUCKET_NAME': 'fashion-search',
-    'AWS_ACCESS_KEY': os.environ.get("YAIN_AWS_S3_ACCESS_KEY"),
-    'AWS_SECRET_KEY': os.environ.get("YAIN_AWS_S3_SECRET_KEY")
-}
-
-DB_CONFIG = {
-    'HOST': 'localhost',
-    'USER': 'root',
-    'PASSWORD': 'root',
+    'AWS_ACCESS_KEY': os.environ.get("YACO_AWS_ADMIN_ACCESS_KEY"),
+    'AWS_SECRET_KEY': os.environ.get("YACO_AWS_ADMIN_SECRET_KEY")
 }
