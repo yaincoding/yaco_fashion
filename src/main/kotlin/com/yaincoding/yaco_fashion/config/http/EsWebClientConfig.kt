2fe3fa85fdad9dbcc7f6ba92915e62a745d90ed5
yaincoding <yaincoding@gmail.com>
2022-10-11 02:04:21 +0900

    unused import 제거

--- src/main/kotlin/com/yaincoding/yaco_fashion/config/http/EsWebClientConfig.kt
+++ src/main/kotlin/com/yaincoding/yaco_fashion/config/http/EsWebClientConfig.kt
@@ -1,15 +1,12 @@
 package com.yaincoding.yaco_fashion.config.http
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.netty.channel.ChannelOption
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mapping.model.CamelCaseSplittingFieldNamingStrategy
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 import java.time.Duration
