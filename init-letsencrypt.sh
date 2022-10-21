4513b69b4507a0fce32ae2c6926162542b546ed3
yaincoding <yaincoding@gmail.com>
2022-10-22 00:37:57 +0900

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(ec2-3-35-148-93.ap-northeast-2.compute.amazonaws.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
